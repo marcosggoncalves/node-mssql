1b623d0aa738f1e2ddc9f66a4b140bee82b47123
Marcos Gonçalves <marcoslopesg7@gmail.com>
2023-09-22 21:05:29 -0300

    Refatorando código.

--- controllers/Veiculos.js
+++ controllers/Veiculos.js
@@ -1,26 +1,29 @@
 
 
+const connect = require('../database');
 
-const listagem = async (req,res) => {
+const listagem = async (req, res) => {
     try {
         const result = await connect.query('SELECT * FROM veiculo where is_site = 1  order by id desc');
 
-        res.render('Veiculos', { title: 'Meu Veiculos', veiculos: result.recordset }); 
+        const { recordset } = result;
+
+        res.render('Veiculos', { title: 'Meu Veiculos', veiculos: recordset });
     } catch (err) {
         res.status(500).send('Erro ao buscar registros.');
     }
 }
 
-const cadastrar = async(req, res) => {
+const cadastrar = async (req, res) => {
     try {
         await connect.connect();
 
         const veiculo = req.body;
 
-        if(veiculo && veiculo.is_site == 0){
+        if (veiculo && veiculo.is_site == 0) {
             await connect.query(`delete from dbo.veiculo where id = ${veiculo.id}`);
-            res.send('Cadastro removido foi removido site!');
-            return;
+
+            return res.send('Cadastro foi removido site!');
         }
 
         await connect.request()
@@ -33,11 +36,11 @@ const cadastrar = async(req, res) => {
             .input('is_site', veiculo.is_site)
             .query('INSERT INTO dbo.veiculo (placa, descricao, url_imagem, marca, modelo, quantidade_lugares, is_site) VALUES (@placa, @descricao, @url_imagem, @marca, @modelo, @quantidade_lugares, @is_site)');
 
-        res.send('Cadastro enviado e postado no site!'); 
+        res.send('Cadastro enviado e postado no site!');
     } catch (err) {
-        res.status(500).send('Não foi possivel enviar cadastrado e postar!');
+        res.status(500).send('Não foi possivel enviar cadastro para o site!');
     }
-} 
+}
 
 module.exports = {
     listagem,
--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const routes = require('./routers');
+const routesVeiculos = require('./routers/Veiculos');
 
 app.use(express.json());
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.use('/', routes);
+app.use('/veiculos', routesVeiculos);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
