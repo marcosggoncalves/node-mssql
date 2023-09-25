f9100389bb36c9394f607d70c24c2ab387e5ff4e
Marcos Lopes Gonçalves <marcos.goncalves@solmais.com.br>
2023-09-25 08:11:00 -0300

    Correção na importação do arquivo

--- routers/Veiculos.js
+++ routers/Veiculos.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {listagem, cadastrar} = require('./controllers/Veiculos');
+const {listagem, cadastrar} = require('../controllers/Veiculos');
 
 router.get('/', listagem);
 router.post('/', cadastrar);
